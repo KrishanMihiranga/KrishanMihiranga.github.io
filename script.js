da41b7fb8da951cb7042d25518415b62083b0674
Xp3cto <xp3cto95@gmail.com>
2023-02-21 04:20:31 +0530

    linked socials

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ var typed = new Typed(".multiple-text", {
     backDelay: 1000,
     loop: true
 })
+
+document.getElementById("git").addEventListener("click", function(event) {
+    event.preventDefault();
+    window.open("https://github.com/KrishanMihiranga", "_blank");
+  });
+  document.getElementById("codePen").addEventListener("click", function(event) {
+    event.preventDefault();
+    window.open("https://codepen.io/mkrishan", "_blank");
+  });
+  document.getElementById("instagram").addEventListener("click", function(event) {
+    event.preventDefault();
+    window.open("https://instagram.com/xxp3cto", "_blank");
+  });
+  document.getElementById("linkedIn").addEventListener("click", function(event) {
+    event.preventDefault();
+    window.open("https://linkedin.com/in/krishan mihiranga", "_blank");
+  });
+  
