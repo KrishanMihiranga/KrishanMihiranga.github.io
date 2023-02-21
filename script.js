7f96a7c70ee630a51468a7e067b7185eb5125e4d
Xp3cto <xp3cto95@gmail.com>
2023-02-21 17:03:48 +0530

    added more functions

--- script.js
+++ script.js
@@ -23,3 +23,16 @@ document.getElementById("git").addEventListener("click", function(event) {
     window.open("https://linkedin.com/in/krishan mihiranga", "_blank");
   });
   
+  var tablinks = document.getElementsByClassName("tab-links");
+  var tabcontents = document.getElementsByClassName("tab-contents");
+
+  function opentab(tabname){
+    for(tablink of tablinks){
+      tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+      tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+  }
